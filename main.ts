a590f07ce05ae5eaae4a8e6aa51c3338e9b5868b
HamishNakatomi <60995335+HamishNakatomi@users.noreply.github.com>
2022-09-06 15:24:05 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
 input.onButtonPressed(Button.A, function () {
+    flip_count = 0
+    count = 0
     if (start == 0) {
-        radio.sendValue("start", 1)
-        count = 0
         start = 1
+        radio.sendValue("start", 1)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(count)
+    basic.showNumber(flip_count)
 })
 radio.onReceivedValue(function (name, value) {
-    count += value
+    flip_count += value
+    music.playTone(175, music.beat(BeatFraction.Sixteenth))
 })
-let count = 0
 let start = 0
+let count = 0
+let flip_count = 0
 radio.setGroup(10)
+radio.setTransmitPower(7)
 loops.everyInterval(1000, function () {
     if (start) {
-        if (count < 10) {
+        if (count < 30) {
             count += 1
             music.playTone(262, music.beat(BeatFraction.Sixteenth))
         } else {
